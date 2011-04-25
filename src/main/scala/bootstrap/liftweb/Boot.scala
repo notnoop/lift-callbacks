fd92a4c0c1800250e205f203d8e38dbbfd2ac789
notnoop <mahmood@notnoop.com>
2011-04-25 09:47:26 -0400

    Remove static folder from SiteMap

--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -21,21 +21,14 @@ class Boot {
 
     // Build SiteMap
     val entries = List(
-      Menu.i("Home") / "index", // the simple way to declare a menu
-
-      // more complex because this menu allows anything in the
-      // /static path to be visible
-      Menu(Loc("Static", Link(List("static"), true, "/static/index"), 
-	       "Static Content")))
-
-    // set the sitemap.  Note if you don't want access control for
+      Menu.i("Home") / "index") // the simple way to declare a menu
     // each page, just comment this line out.
     LiftRules.setSiteMap(SiteMap(entries:_*))
 
     //Show the spinny image when an Ajax call starts
     LiftRules.ajaxStart =
       Full(() => LiftRules.jsArtifacts.show("ajax-loader").cmd)
-    
+
     // Make the spinny image go away when it ends
     LiftRules.ajaxEnd =
       Full(() => LiftRules.jsArtifacts.hide("ajax-loader").cmd)
